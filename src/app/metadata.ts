ba23a0b6fd5518fd1e3dfc7789cf88680dba7e0b
kaiznanji <kaiznanji@gmail.com>
2025-05-29 16:14:31 -0400

    minor fixes, added logo and name to tab

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,6 +1,11 @@
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: "ApplyWise",
+  title: "Applywise Jobs",
   description: "Application management platform",
+  icons: {
+    icon: '/images/logo_icon_dark.svg',
+    shortcut: '/images/logo_icon_dark.svg',
+    apple: '/images/logo_icon_dark.svg',
+  },
 }; 
